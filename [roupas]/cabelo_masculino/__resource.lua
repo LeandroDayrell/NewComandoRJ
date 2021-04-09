7bd48771ccedd9d314fa7aa891cf542118d20d30
Leandro Dayrell Silva <leandrodayrell@live.com>
2021-04-09 02:14:58 -0300

    att
    att

---
+++ [roupas]/cabelo_masculino/__resource.lua
@@ -0,0 +1,2 @@
+client_script "@vrp/lib/lib.lua" --Para remover esta pendencia de todos scripts, execute no console o comando "uninstall"
+
---
+++ [roupas]/extraSlots/fxmanifest.lua
@@ -0,0 +1,6 @@
+client_script "@vrp/lib/lib.lua" --Para remover esta pendencia de todos scripts, execute no console o comando "uninstall"
+
+fx_version 'bodacious'
+game 'gta5'
+
+-- (C) 2020 - Extra Slots by Ana Clara Rodrigues. All rights reserved.
