147562c6afd5fcbcc684e141d784b54f90e57fba
Leandro Dayrell Silva <leandrodayrell@live.com>
2021-06-23 04:22:41 -0300

    Atualizacao Corolla
    Adiciionado corolla

--- [Veiculos Brs]/Corolla_New/__resource.lua
+++ [Veiculos Brs]/Corolla_New/__resource.lua
@@ -4,14 +4,12 @@ files {
     'vehicles.meta',
     'carvariations.meta',
     'carcols.meta',
-    'handling.meta',    
-	'dlctext.meta',	
-	
+    'handling.meta',
+    'vehiclelayouts.meta',
 }
- 
+
 data_file 'HANDLING_FILE' 'handling.meta'
 data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 data_file 'CARCOLS_FILE' 'carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
-data_file 'DLC_TEXT_FILE' 'dlctext.meta'
-client_script 'vehicle_names.lua'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
